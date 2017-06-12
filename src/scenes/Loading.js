229b618f4b0c00aac98183add9a9f219894f2eb9
conraddavisjr <conraddavisjr@msn.com>
2017-06-12 14:37:38 -0700

    implemented spinning styles on the loader

--- src/scenes/Loading.js
+++ src/scenes/Loading.js
@@ -1,55 +1,18 @@
 import React, { Component } from 'react';
 import {
-  View,
-  Animated,
-  Easing,
-  TouchableHighlight,
-  Text
+  View
 } from 'react-native';
+
+import LoadingSpinner from '../components/LoadingSpinner';
 
 class Loading extends Component {
 
-  constructor() {
-    super();
-
-    this.state = {
-      spinValue: new Animated.Value(0),  // Initial value for opacity: 0
-    }
-
-    this.runAnimation = this.runAnimation.bind(this);
-  }
-
-  componentDidMount() {
-    console.log('mounted Loading', this.props);
-    this.runAnimation();
-  }
-
-  runAnimation() {
-    console.log('runAnimation Called!')
-    Animated.timing(
-      this.state.spinValue,
-      {
-        toValue: 1,
-        duration: 3000,
-        easing: Easing.linear
-      }
-    ).start()
-  }
-
   render() {
-    const { globalStyles, app } = this.props;
-    const spin = this.state.spinValue.interpolate({
-      inputRange: [0, 1],
-      outputRange: ['0deg', '360deg']
-    })
+    const { globalStyles } = this.props;
 
     return (
-      <View style={[globalStyles.container, { backgroundColor: 'aqua' }]}>
-        <Animated.Image
-          style={{transform: [{rotate: spin}] }}
-          source={require('../../images/logo.png')} 
-          resizeMode='contain'
-        />
+      <View style={[globalStyles.container]}>
+        <LoadingSpinner />
       </View>
     );
   };
