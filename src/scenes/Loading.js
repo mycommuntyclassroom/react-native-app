291b5d3e85773c37da8b481d31895d54030c67b5
conraddavisjr <conraddavisjr@msn.com>
2017-06-12 14:22:08 -0700

    making the loader rotate

--- src/scenes/Loading.js
+++ src/scenes/Loading.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import {
   View,
-  ActivityIndicator,
+  Animated,
+  Easing,
   TouchableHighlight,
   Text
 } from 'react-native';
@@ -11,30 +12,44 @@ class Loading extends Component {
   constructor() {
     super();
 
-    this.onPress = this.onPress.bind(this);
-  }
+    this.state = {
+      spinValue: new Animated.Value(0),  // Initial value for opacity: 0
+    }
 
-  onPress() {
-    this.props.app.goToScene('welcome', { sup: true });
-    // this.props.app.goToScene('hosts', { hostID: 123 });
+    this.runAnimation = this.runAnimation.bind(this);
   }
 
   componentDidMount() {
     console.log('mounted Loading', this.props);
+    this.runAnimation();
+  }
+
+  runAnimation() {
+    console.log('runAnimation Called!')
+    Animated.timing(
+      this.state.spinValue,
+      {
+        toValue: 1,
+        duration: 3000,
+        easing: Easing.linear
+      }
+    ).start()
   }
 
   render() {
     const { globalStyles, app } = this.props;
+    const spin = this.state.spinValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '360deg']
+    })
+
     return (
       <View style={[globalStyles.container, { backgroundColor: 'aqua' }]}>
-        <Text style={globalStyles.title}>
-          Loading
-        </Text>
-        <ActivityIndicator
-          animating={true}
-          style={globalStyles.loader}
-          size="large"
-          color="purple" />
+        <Animated.Image
+          style={{transform: [{rotate: spin}] }}
+          source={require('../../images/logo.png')} 
+          resizeMode='contain'
+        />
       </View>
     );
   };
