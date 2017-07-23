8e26c89ff1de279409647c1702745a6ac11760a3
conraddavisjr <conraddavisjr@msn.com>
2017-07-22 20:47:04 -0700

    building event detail view

---
+++ src/components/EventDetailsView/style.js
@@ -0,0 +1,4 @@
+const EventDetailsView = {}
+
+
+export default EventDetailsView;
---
+++ src/scenes/EventDetails.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import {
+  View,
+  TouchableHighlight,
+  Text
+} from 'react-native';
+
+import Header from '../components/Header';
+import EventDetailsView from '../components/EventDetailsView';
+
+class EventDetails extends Component {
+
+  constructor() {
+    super();
+    console.log('EventDetails constructor');
+  }
+
+  componentDidMount() {
+    console.log('mounted EventDetails', this.props);
+  }
+
+  render() {
+    const props = this.props
+    const { globalStyles, app } = props;
+
+    return (
+      <View style={{marginBottom: 90, backgroundColor: 'white'}}>
+        <Header { ...props } />
+        <EventDetailsView {...props} />
+      </View>
+    );
+  };
+
+}
+
+export default EventDetails;
+
