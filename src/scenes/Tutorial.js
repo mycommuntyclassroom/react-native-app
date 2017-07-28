349eca2b48dd3fcaa91c4363b24a9182a6b51f5f
conraddavisjr <conraddavisjr@msn.com>
2017-07-28 12:00:25 -0700

    added the tut

--- src/scenes/Tutorial.js
+++ src/scenes/Tutorial.js
@@ -5,57 +5,41 @@ import {
   Text,
   Image
 } from 'react-native';
-import Carousel from 'react-native-snap-carousel';
+import { IndicatorViewPager, PagerDotIndicator } from 'rn-viewpager'
 import Button from '../components/Button';
 import { deviceDimensions } from '../styles';
 
 class Tutorial extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      nextBtnName: 'Continue',
-      slideIndex: 0
-    };
-
-    this.next = this.next.bind(this)
   }
 
-  next(slideIndex) {
-    const { app } = this.props;
-    this.state.slideIndex === 4 
-      // go to the dashboard scene
-      ? app.goToScene('Dashboard', {app})
-      // progress the slider by the index value
-      : this._carousel.snapToNext()
+  _renderDotIndicator () {
+    return <PagerDotIndicator pageCount={5}
+                              selectedDotStyle={{width: 13, borderRadius: 6, backgroundColor: '#fff'}}
+                              dotStyle={{width:5, height:5, borderRadius:2.5,margin:5,borderWidth:1,
+                              borderColor:'hsla(0,0%,100%,.5)', backgroundColor:'transparent' }}/>
   }
 
   render() {
-    const { globalStyles, app } = this.props;
 
-    const getSlideIndex = (slideIndex) => {
-      // update the state with the index of the slider
-      this.setState({slideIndex});
-      slideIndex === 4 && this.setState({nextBtnName: 'Get Started'})
-    }
-
+    const { app } = this.props;
     const imageDimensions = { width: deviceDimensions.deviceWidth, height: 500 };
 
     return (
       <View style={{ height:'100%', backgroundColor: '#74bcf7', paddingTop:20, paddingBottom:20}}>
-        <Carousel
-          ref={(carousel) => { this._carousel = carousel; }}
-          sliderWidth={deviceDimensions.deviceWidth}
-          itemWidth={deviceDimensions.deviceWidth}
-          onSnapToItem={(slideIndex) => { getSlideIndex(slideIndex) }}
+        <IndicatorViewPager
+          style={{flex:0.8}}
+          indicator={this._renderDotIndicator()}
         >
           <View><Image source={require('../../images/intro-tut-card-0.png')} resizeMode='contain' style={imageDimensions} /></View>
           <View><Image source={require('../../images/intro-tut-card-1.png')} resizeMode='contain' style={imageDimensions} /></View>
           <View><Image source={require('../../images/intro-tut-card-2.png')} resizeMode='contain' style={imageDimensions} /></View>
           <View><Image source={require('../../images/intro-tut-card-3.png')} resizeMode='contain' style={imageDimensions} /></View>
-          <View><Image source={require('../../images/intro-tut-card-4.png')} resizeMode='contain' style={imageDimensions} /></View>
-        </Carousel>
-        <Button text={this.state.nextBtnName} onPress={ (slideIndex) => {this.next(slideIndex)} }></Button>
+          <View><Image source={require('../../images/intro-tut-card-4.png')} resizeMode='contain' style={imageDimensions} />
+            <View style={{ margin: 20 }}><Button text='Get Started' onPress={() => app.goToScene('Dashboard', {app}) }></Button></View>
+          </View>
+        </IndicatorViewPager>
       </View>
     );
   }
